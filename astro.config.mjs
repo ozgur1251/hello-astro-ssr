229cec9c98da8a430bd0d91d458655898833cde3
ozgur1251 <ozgurcalman@gmail.com>
2025-04-21 18:42:35 +0300

    Explicitly set host and port in Node adapter config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   adapter: node({
-    mode: 'standalone'
+    mode: 'standalone',
+    host: '0.0.0.0', // Açıkça tüm arayüzleri dinlemesini söyleyelim
+    port: 4321 // Portu da burada belirtelim (Dockerfile EXPOSE ile aynı)
   })
 });
